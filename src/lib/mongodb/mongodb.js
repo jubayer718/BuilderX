d642ff036eb86c64a1a424d9332451371ee812c0
jubayer718 <jubayerahmed2462@gmail.com>
2025-11-01 08:20:50 +0600

    project initial setup and mongodb connection done

---
+++ src/lib/mongodb/mongodb.js
@@ -0,0 +1,23 @@
+import { mongoose } from 'mongoose';
+const MONGODB_URI = process.env.MONGODB_URI;  
+
+if (!MONGODB_URI) {
+  throw new Error('MONGODB_URI is not found from environment variables');
+}
+
+
+
+const StartServer = async () => {
+  try {
+    await mongoose.connect(MONGODB_URI);
+    console.log('Connected to MongoDB');
+
+  } catch (error) {
+    console.error('Error connecting to MongoDB:', error);
+  }
+}
+
+
+(async () => {
+  await StartServer();  
+})();
